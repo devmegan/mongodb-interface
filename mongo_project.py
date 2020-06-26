2f1730f211c23be43e3f80fcbc287150bb20ddaf
mvmolloy <mvamolloy@gmail.com>
2020-06-26 08:21:49 +0000

    tidying up python code

--- mongo_project.py
+++ mongo_project.py
@@ -3,12 +3,13 @@ import os
 """ get env.py file for keys/URIs"""
 from os import path
 if path.exists("env.py"):
-  import env 
-  print("env.py imported")
+    import env
+    print("env.py imported")
 
-MONGO_URI = os.environ.get('MONGO_URI') #get mongo URI from env.py
-DB_NAME = "myTestDB" # database name
-COLL_NAME = "myFirstMDB" # collection name
+MONGO_URI = os.environ.get('MONGO_URI')  # get mongo URI from env.py
+DB_NAME = "myTestDB"  # database name
+COLL_NAME = "myFirstMDB"  # collection name
+
 
 def mongo_connect(url):
     """ connect to my mongodb """
@@ -18,7 +19,8 @@ def mongo_connect(url):
         return conn
     except pymongo.errors.ConnectionFailure as e:
         # if connection fails, let user know
-        print ("Could not connect to Mongo %s") % e #interpolates error message into string. 
+        print("Could not connect to Mongo %s") % e  # interpolates error message into string.
+
 
 def show_menu():
     """display menu options to user"""
@@ -32,10 +34,11 @@ def show_menu():
     option = input("Enter Option: ")
     return option
 
-def main_loop(): 
+
+def main_loop():
     """ call menu every time we come back to it """
-    while True: # will basically run forever...
-        option = show_menu() # store result of show_menu function in variable option
+    while True:  # will basically run forever...
+        option = show_menu()  # store result of show_menu function in variable option
         if option == "1":
             print("You have selected option 1")
         elif option == "2":
@@ -52,10 +55,11 @@ def main_loop():
             print("Invalid Option")
             print("")
 
-conn = mongo_connect(MONGO_URI) # call function to connect to mongodb
 
-coll = conn[DB_NAME][COLL_NAME] # set connection 
+conn = mongo_connect(MONGO_URI)  # call function to connect to mongodb
+
+coll = conn[DB_NAME][COLL_NAME]  # set connection
 print("\n"*3 + str(coll) + "\n"*3)
 
 
-main_loop() #call main loop to continue displaying options/processing user input
+main_loop()  # call main loop to continue displaying options/processing user input
